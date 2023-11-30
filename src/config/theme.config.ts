fb05a4db8c4b05e3c9237ff4a36735e0bda3ae80
Julian Lucero <julianlucero2012@gmail.com>
2023-11-30 02:59:59 -0300

    chore: Added mui theme config

---
+++ src/config/theme.config.ts
@@ -0,0 +1,23 @@
+export const THEME_CONFIG = {
+  typography: {
+    htmlFontSize: 16,
+    fontFamily: "Roboto",
+    h1: {
+      fontSize: "3rem",
+      fontWeight: "400",
+    },
+    h2: {
+      fontSize: "1.25rem",
+      fontWeight: "500",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 480,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+    },
+  },
+};
